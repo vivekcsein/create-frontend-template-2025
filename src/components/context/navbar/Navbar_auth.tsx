d71843f732d79cc024be956acc32fbfa9343ad63
vivek <vivekcse.in@gmail.com>
2025-07-05 20:08:51 +0530

    navbar generated

---
+++ src/components/context/navbar/Navbar_auth.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar_auth = () => {
+  return <div>Navbar_auth</div>;
+};
+
+export default Navbar_auth;
---
+++ src/components/context/navbar/Navbar_user.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar_user = () => {
+  return <div>Navbar_user</div>;
+};
+
+export default Navbar_user;
